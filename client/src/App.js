576860c5719a7e493003646d1163fa90954d4b86
Colin Peters <colinthedev@gmail.com>
2021-10-26 17:35:22 -0600

    Express server interacting with react frontend

---
+++ client/src/App.js
@@ -0,0 +1,103 @@
+// GET
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    renderedResponse: ''
+  }
+
+  getResponse = async () => {
+    const response = await fetch('/api/hello')
+    const body = await response.json()
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  }
+
+  componentDidMount() {
+    this.getResponse()
+      .then(res => {
+        const someData = res;
+        this.setState({ renderedResponse: someData })
+    })
+  }
+  
+
+  render() {
+    const { renderedResponse } = this.state
+
+    return (
+      <div className="App">
+        <h2>Call out to API</h2>
+        <p>{ renderedResponse.express }</p>
+      </div>
+    )
+  }
+}
+
+export default App;
+
+// GET AND POST
+// import React, { Component } from 'react';
+
+// import './App.css';
+
+// class App extends Component {
+//   state = {
+//     response: '',
+//     post: '',
+//     responseToPost: '',
+//   };
+
+//   componentDidMount() {
+//     this.callApi()
+//       .then(res => this.setState({ response: res.express }))
+//       .catch(err => console.log(err));
+//   }
+
+//   callApi = async () => {
+//     const response = await fetch('/api/hello');
+//     const body = await response.json();
+//     if (response.status !== 200) throw Error(body.message);
+
+//     return body;
+//   };
+
+//   handleSubmit = async e => {
+//     e.preventDefault();
+//     const response = await fetch('/api/world', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ post: this.state.post }),
+//     });
+//     const body = await response.text();
+
+//     this.setState({ responseToPost: body });
+//   };
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <p>{this.state.response}</p>
+//         <form onSubmit={this.handleSubmit}>
+//           <p>
+//             <strong>Post to Server:</strong>
+//           </p>
+//           <input
+//             type="text"
+//             value={this.state.post}
+//             onChange={e => this.setState({ post: e.target.value })}
+//           />
+//           <button type="submit">Submit</button>
+//         </form>
+//         <p>{this.state.responseToPost}</p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
