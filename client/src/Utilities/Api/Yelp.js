90f4316f5a83f3a7bc453682d99070e7e6eefa15
Colin Peters <colinthedev@gmail.com>
2021-12-11 14:40:06 -0700

    Added https to fetch url

--- client/src/Utilities/Api/Yelp.js
+++ client/src/Utilities/Api/Yelp.js
@@ -2,7 +2,7 @@ const { default: SearchBar } = require("../../components/SearchBar/SearchBar");
 
 const Yelp = {
     searchYelp(term, location) {
-        return fetch(`/api/hello?term=${term}&location=${location}`)
+        return fetch(`https://api/hello?term=${term}&location=${location}`)
             .then((response) => {
                 // console.log(response)
                 return response.json()
